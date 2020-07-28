6cedf84d58eec826d202c119ae629b70a691880f
Romal George <romalgeorge3@gmail.com>
2020-07-28 17:00:42 -0400

    Added multiple enemies---------------

--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -3,7 +3,9 @@ module scenes{
         //variables
         private background:objects.Background;
         private player:objects.Player;
-        private enemy:objects.Enemy;
+        //private enemy:objects.Enemy;
+        private enemies:objects.Enemy[];
+        private enemyNum:number;
 
         //constructor
         constructor(assetManager:createjs.LoadQueue){
@@ -15,20 +17,31 @@ module scenes{
             //initialize variables
             this.background=new objects.Background(this.assetManager);
             this.player=new objects.Player(this.assetManager);
-            this.enemy=new objects.Enemy(this.assetManager);
+            //this.enemy=new objects.Enemy(this.assetManager);
+            this.enemies = new Array<objects.Enemy>();
+            this.enemyNum = 5;
+            for(let i = 0; i< this.enemyNum; i++){
+                this.enemies[i]=new objects.Enemy(this.assetManager);
+            }
             this.Main();
         }
 
         public Update():void{
             //this.background.Update();
             this.player.Update();
-            this.enemy.Update();
+            //this.enemy.Update();
+            this.enemies.forEach(e => {
+                e.Update();
+            })
         }
 
         public Main():void{
             this.addChild(this.background);
             this.addChild(this.player);
-            this.addChild(this.enemy);
+            //this.addChild(this.enemy);
+            this.enemies.forEach(e => {
+                this.addChild(e);
+            })
         }
     }
 }
