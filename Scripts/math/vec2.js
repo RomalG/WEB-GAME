f883452e36b0aa1434f984f3e9e2d421411aa892
Romal George <romalgeorge3@gmail.com>
2020-07-30 15:22:46 -0400

    Added math function..............................................

---
+++ Scripts/math/vec2.js
@@ -0,0 +1,32 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var math;
+(function (math) {
+    var Vec2 = /** @class */ (function (_super) {
+        __extends(Vec2, _super);
+        //variable
+        //constructor
+        function Vec2(x, y) {
+            if (x === void 0) { x = 0; }
+            if (y === void 0) { y = 0; }
+            return _super.call(this, x, y) || this;
+        }
+        //Methods
+        Vec2.Distance = function (P1, P2) {
+        };
+        return Vec2;
+    }(createjs.Point));
+    math.Vec2 = Vec2;
+})(math || (math = {}));
+//# sourceMappingURL=vec2.js.map
